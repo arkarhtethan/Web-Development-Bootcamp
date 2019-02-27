968e4b69f3dbf3959919c48b60b0666e887c6095
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2019-02-27 21:22:11 +0800

    commiting latest work for challenge 6

--- app.js
+++ app.js
@@ -2,13 +2,33 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
 
 // Have to also send the other files that were used locally. Put them in the folder called public.
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
