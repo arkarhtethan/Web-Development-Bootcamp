3172fcd2b843b67c9642f44b245cd0eeec2b160c
Simon Tan Yu Jing <simon.tan.yu.jing@gmail.com>
2019-03-16 21:40:43 +0800

    prepared todolist v2 for deploying on heroku with atlus

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-simon:Test123@cluster0-mkoix.mongodb.net/todolistDB", {
   useNewUrlParser: true
 });
 
